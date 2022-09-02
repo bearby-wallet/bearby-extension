78c8ca797f1462fdd7b50639f963f85b41cd1eb8
Rinat <lich666black@gmail.com>
2022-09-02 14:06:54 +0300

    added ExecuteSmartContractBuild class

--- core/background/provider/transaction.ts
+++ core/background/provider/transaction.ts
@@ -1,3 +1,4 @@
+import { toByteArray } from "base64-js";
 import { base58Decode, isBase58Address } from "lib/address";
 import { assert } from "lib/assert";
 import { VarintEncode } from 'lib/varint';
@@ -40,6 +41,56 @@ export class PaymentBuild {
       ...typeIdEncoded,
       ...recipient,
       ...amount
+    ]);
+  }
+}
+
+
+export class ExecuteSmartContractBuild {
+  static operation = OperationsType.ExecuteSC;
+
+  contractByteCode: Uint8Array;
+  byteCodeLength: Uint8Array;
+  gasLimit: number;
+  amount: number;
+  gasPrice: number;
+  fee: number;
+  expirePeriod: number
+
+  constructor(
+    fee: number,
+    expirePeriod: number,
+    contractDataBase64: string,
+    gasLimit: number,
+    amount: number,
+    gasPrice: number
+  ) {
+    this.contractByteCode = toByteArray(contractDataBase64);
+    this.byteCodeLength = new VarintEncode().encode(this.contractByteCode.length);
+    this.gasLimit = gasLimit;
+    this.amount = amount;
+    this.gasPrice = gasPrice;
+    this.fee = fee;
+    this.expirePeriod = expirePeriod;
+  }
+
+  async bytes() {
+    const fee = new VarintEncode().encode(this.fee);
+		const expirePeriod = new VarintEncode().encode(this.expirePeriod);
+		const typeIdEncoded = new VarintEncode().encode(ExecuteSmartContractBuild.operation);
+    const amount = new VarintEncode().encode(this.amount);
+    const gasPrice = new VarintEncode().encode(this.gasPrice);
+    const gasLimit = new VarintEncode().encode(this.gasLimit);
+
+    return Uint8Array.from([
+      ...fee,
+      ...expirePeriod,
+      ...typeIdEncoded,
+      ...gasLimit,
+      ...amount,
+      ...gasPrice,
+      ...this.byteCodeLength,
+      ...this.contractByteCode
     ]);
   }
 }
