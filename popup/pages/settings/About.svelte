940cff3af65da52ee72b40361607dae025717c1c
Rinat <lich666black@gmail.com>
2022-10-04 13:53:35 +0300

    added about page #5

---
+++ popup/pages/settings/About.svelte
@@ -0,0 +1,88 @@
+<script lang="ts">
+	import { _ } from 'popup/i18n';
+
+	import { Runtime } from 'lib/runtime';
+
+	import NavClose from '../../components/NavClose.svelte';
+
+  const manifest = Runtime.runtime.getManifest(); 
+
+	const links = [
+		{
+			name: $_('about.issues'),
+			url: 'https://github.com/bearby-wallet/bearby-extension/issues'
+		}
+	];
+
+  console.log(manifest);
+  
+</script>
+
+<main>
+	<NavClose title={$_('about.title')}/>
+  <img
+		src="/imgs/logo.png"
+		alt="logo"
+	>
+	<h2>
+		bearby {$_('about.version')} - {manifest.version}
+	</h2>
+  <h2>
+		{$_('about.manifest_version')} - {manifest.manifest_version}
+	</h2>
+	<ul>
+		{#each links as link}
+			<li>
+				<a
+					href={link.url}
+					target="_blank"
+				>
+					{link.name}
+				</a>
+			</li>
+		{/each}
+	</ul>
+</main>
+
+<style lang="scss">
+	@import "../../styles/mixins";
+
+	main {
+		height: 100vh;
+		@include flex-center-top-column;
+	}
+	h2,
+  ul {
+		z-index: 99;
+	}
+	ul {
+		margin: 0;
+    padding: 0;
+		z-index: 2;
+    margin-top: 30px;
+		& > li {
+			text-align: center;
+		}
+		& > li > a {
+			cursor: pointer;
+			color: var(--text-color);
+			line-height: 30px;
+			font-size: 21pt;
+
+			&:hover {
+				color: var(--primary-color);
+			}
+		}
+	}
+	img {
+		position: fixed;
+		max-width: 900px;
+		width: 130vw;
+		margin: 15vh auto;
+		left: auto;
+		right: auto;
+		top: -47px;
+		opacity: 0.2;
+    z-index: 1;
+	}
+</style>
--- popup/routers/index.ts
+++ popup/routers/index.ts
@@ -14,6 +14,7 @@ import NetwrokPage from '../pages/settings/Netwrok.svelte';
 import AccountPage from '../pages/settings/Account.svelte';
 import GeneralPage from '../pages/settings/General.svelte';
 import ContactsPage from '../pages/settings/Contacts.svelte';
+import AboutPage from '../pages/settings/About.svelte';
 
 import LockPage from '../pages/Lock.svelte';
 import StartPage from '../pages/Start.svelte';
@@ -63,6 +64,12 @@ export default {
   }),
   '/settings': wrap({
     component: SettingsPage,
+    conditions: [
+      routerGuard
+    ]
+  }),
+  '/about': wrap({
+    component: AboutPage,
     conditions: [
       routerGuard
     ]
