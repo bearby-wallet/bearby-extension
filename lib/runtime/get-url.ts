382844f05022a379ebfc8279e8ff533294dda17b
Hicaru <hicc0rus@gmail.com>
2022-10-31 11:32:37 +0300

    fixed manifest

--- lib/runtime/get-url.ts
+++ lib/runtime/get-url.ts
@@ -1,13 +1,6 @@
 import { Runtime } from ".";
-import { getManifestVersion } from "./manifest";
 
 
 export function getExtensionURL(content: string) {
-  const mv = getManifestVersion();
-
-  if (mv === 3) {
-    return Runtime.runtime.getURL(content);
-  }
-
-  return Runtime.extension.getURL(content);
+  return Runtime.runtime.getURL(content);
 }
