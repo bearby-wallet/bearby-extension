7a6cca92ed2cdeea5efd0c29842fbb39e4c6de63
Hicaru <hicc0rus@gmail.com>
2023-03-06 15:29:27 +0500

    fixed testnet 20 address prefix

--- config/common.ts
+++ config/common.ts
@@ -2,13 +2,13 @@ export const TIME_BEFORE_LOCK = 3; // in hours.
 
 export const SECRET_KEY_PREFIX = "S";
 export const VERSION_NUMBER = 0;
-export const ADDRESS_PREFIX = "A";
+export const ADDRESS_PREFIX = "AU";
 export const PUBLIC_KEY_PREFIX = "P";
 export const MAX_TX_QUEUE = 20;
 export const MASSA_DECIMALS = 9;
 export const PERIOD_OFFSET = 5;
-export const ZERO_ADDRESS = 'A11111111111111111111116iowaD';
-export const ROLL_ADDRESS = 'A111111111111111111111Awmv8x';
+export const ZERO_ADDRESS = 'AS11111111111111111111116iowaD';
+export const ROLL_ADDRESS = 'AS111111111111111111111Awmv8x';
 export const PROMT_PAGE = 'index.html';
 export const WORKER_POOLING = 8000; // Seconds
 export const POPUP_HEIGHT = 600;
