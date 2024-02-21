4bb3b18b66e01ae8e609de3241d4b7dba407c776
Hicaru <hicc0rus@gmail.com>
2024-02-21 12:00:45 +0700

    added Disconnected icons

---
+++ popup/components/icons/Disconnected.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  export let width = '34';
+  export let height = '34';
+  export let className = '';
+</script>
+
+<svg
+  width={width}
+  height={height}
+  viewBox="0 0 71 24"
+  fill="none"
+  class={className}
+>
+  <path d="M14.1061 8.88917H0V15.1119H14.1061C15.4748 20.228 20.1167 24 25.6503 24H34.2909V0H25.6503C20.1166 0 15.4736 3.77197 14.1061 8.88917Z" fill="black"/>
+  <path d="M71 8.88917H56.8928C55.5252 3.77197 50.8833 0 45.3497 0H36.7092V24H45.3497C50.8822 24 55.524 20.228 56.8928 15.1119H71V8.88917Z" fill="black"/>
+</svg>  
